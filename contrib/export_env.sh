b489a3833bc19d33686238030a613875d4828ffc
Pavel Loginov <56807054+welel@users.noreply.github.com>
2024-08-31 09:19:04 +0300

    feat: upgrade laod envs script

--- contrib/export_env.sh
+++ contrib/export_env.sh
@@ -2,7 +2,17 @@
 # Use this script to export all variables from .env file
 # $ . export_env.sh /path/to/.env
 
-ENV_FILE=${1:-'.env'}
+VERBOSE=false
+ENV_FILE='.env'
+
+# Iterate over all arguments to check for -v and file path
+for arg in "$@"; do
+    if [[ "$arg" == "-v" ]]; then
+        VERBOSE=true
+    elif [[ "$arg" != "-"* ]]; then
+        ENV_FILE="$arg"
+    fi
+done
 
 if [[ -f "$ENV_FILE" ]]; then
     while IFS= read -r line; do
@@ -11,6 +21,9 @@ if [[ -f "$ENV_FILE" ]]; then
             # Export each variable while trimming spaces around the '='
             var_name=$(echo "$line" | cut -d '=' -f 1 | xargs)
             var_value=$(echo "$line" | cut -d '=' -f 2- | xargs)
+            if [[ "$VERBOSE" == true ]]; then
+                echo "Exporting $var_name=$var_value"
+            fi
             export "$var_name=$var_value"
         fi
     done < "$ENV_FILE"
