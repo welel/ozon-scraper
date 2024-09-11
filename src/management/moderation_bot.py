0f1dbd3f962e21acc0904264d3d5d859eef60075
Pavel Loginov <56807054+welel@users.noreply.github.com>
2024-09-12 00:49:21 +0300

    refactor: split handlers

--- src/management/moderation_bot.py
+++ src/management/moderation_bot.py
@@ -1,12 +1,12 @@
 import asyncio
 import click
 
-from modules.moderation_bot.bot import start
+from modules.moderation_bot.bot import start_bot
 
 
 @click.command("start_moderation_bot", help="Start moderation bot.")
 def start_moderation_bot():
     try:
-        asyncio.run(start())
+        asyncio.run(start_bot())
     except (KeyboardInterrupt, SystemExit):
         click.echo("Moderation bot stopped.")
---
+++ src/modules/moderation_bot/config.py
@@ -0,0 +1,4 @@
+import os
+
+API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+NUM_LABELS = 5
